d590bcc624a1994ab8fd904aaae634db7f5621d8
raquezha <rarwkk@gmail.com>
2018-09-06 03:52:34 +0800

    added show/hide keyboard support for Activity

--- playground/src/main/java/net/raquezha/playground/extensions/KeyboardExt.kt
+++ playground/src/main/java/net/raquezha/playground/extensions/KeyboardExt.kt
@@ -17,6 +17,20 @@ fun Activity.setKeyboardVisibilityListener(onKeyboardVisibilityListener: OnKeybo
     }
 }
 
+
+fun Activity.hideKeyboard() {
+    val rootView = window.decorView.rootView
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(rootView.windowToken, 0)
+}
+
+fun Activity.showKeyboard() {
+    val rootView = window.decorView.rootView
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.showSoftInput(rootView, InputMethodManager.SHOW_IMPLICIT)
+    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+}
+
 fun View.hideKeyboard() {
     val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
@@ -28,3 +42,4 @@ fun View.showKeyboard() {
     inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
     (context as Activity).window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
 }
+
